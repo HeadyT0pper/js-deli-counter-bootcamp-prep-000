5cba9779c75a9256f86127e17f544118ebd8a98d
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-21 16:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${name}`)
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+var x = 1
+function takeANumberTwo(katzDeliLine) {
+  x++
+  katzDeliLine.push(x)
+}
 function nowServing(katzDeliLine) {
-  let i = 0
-  while (i < katzDeliLine.length) {
-    i++
-  } 
-  if (i === 0) {
-  return "There is nobody waiting to be served!"
-  }
-  else {
-  return (`Currently serving ${katzDeliLine.shift()}.`)
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0, 1);
+    return "Currently serving " + name + ".";
   }
 }
-
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
